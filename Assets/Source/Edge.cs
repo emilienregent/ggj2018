ef179e04b2cedd77e15130d1772725f6388129b6
MonsieurTweek <gaetan.regent@gmail.com>
2018-01-27 00:31:36 +0100

    A POC Split Screen

---
+++ Assets/Source/Edge.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Edge : MonoBehaviour {
+
+    private Camera[] cams;
+    public Camera currentCam;
+
+	// Use this for initialization
+	void Start ()
+    {
+
+        cams = Camera.allCameras;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Vector3 viewPos = currentCam.WorldToViewportPoint(transform.position);
+
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Vector3 position = transform.position;
+            position.x--;
+            transform.position = position;
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Vector3 position = transform.position;
+            position.x++;
+            transform.position = position;
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Vector3 position = transform.position;
+            position.z++;
+            transform.position = position;
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Vector3 position = transform.position;
+            position.z--;
+            transform.position = position;
+        }
+
+        Vector3 viewPos = currentCam.WorldToViewportPoint(transform.position);
+
+    }
+}
---
+++ Assets/Source/Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Vector3 position = transform.position;
+            position.x--;
+            transform.position = position;
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Vector3 position = transform.position;
+            position.x++;
+            transform.position = position;
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Vector3 position = transform.position;
+            position.z++;
+            transform.position = position;
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Vector3 position = transform.position;
+            position.z--;
+            transform.position = position;
+        }
+
+
+
+    }
+}

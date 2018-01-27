6091c7e2c5f802f8e66b9d9237fa30ea3d4740b9
Maxeee <maxbinetruy@gmail.com>
2018-01-27 02:51:57 +0100

    LevelGenerator
    Could read an image (white=walkable, black=not walkable) to instantiate all dungeon tiles

---
+++ Assets/Editor/Environment/LevelGeneratorEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(LevelGenerator))]
+public class LevelGeneratorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        LevelGenerator levelGenerator = (LevelGenerator)target;
+        if (GUILayout.Button("Generate Level"))
+        {
+            levelGenerator.GenerateLevel();
+        }
+    }
+}

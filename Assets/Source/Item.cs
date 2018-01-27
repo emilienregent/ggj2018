d15372bebf7ee2a8ee53e9adc823e567edad938b
JejeG <garciajeromejeje@hotmail.fr>
2018-01-27 23:33:48 +0100

    Kick potions

--- Assets/Source/Item.cs
+++ Assets/Source/Item.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 abstract public class Item : MonoBehaviour {
     private const float INITIAL_SPEED = 5f;
@@ -15,26 +16,67 @@ abstract public class Item : MonoBehaviour {
     private float _speed = INITIAL_SPEED;
     private float _kickSpeed = INITIAL_SPEED;
     private float _kickTimer = 0f;
-
+    private bool _isWarping = false;
+    private DirectionEnum _warpingDirection = DirectionEnum.NONE;
     public float rotationSpeed;
+    private PlayerController _player = null;
+    private int _currentDungeonId = 0;
+    protected NavMeshAgent _agent = null;
 
     public abstract void ActiveItem(PlayerController player);
+
+
+    public void SetPlayer(PlayerController playerController) {
+        _player = playerController;
+
+        _currentDungeonId = _player.dungeon.id;
+    }
 
     protected void Move() {
         if(canBeKick && _isKicked == true)
         {
             transform.position += _direction * _speed * Time.deltaTime;
-            _speed = _kickSpeed * _speedCurve.Evaluate(_kickTimer);
-            _isKicked = _speed > 0f;
-            _kickTimer += Time.deltaTime;
 
-            // Just stop to be kicked out
-            if(_isKicked == false)
+            if(_player.dungeon == null)
             {
-                // Reset speed
-                _speed = INITIAL_SPEED / _weight;
+                UnityEngine.Assertions.Assert.IsNotNull(_player, "Player is null when kicking enemy " + this);
+                UnityEngine.Assertions.Assert.IsNotNull(_player.dungeon, "Player " + _player.playerId + " doesn't have a dungeon when kicking enemy " + this);
+                UnityEngine.Assertions.Assert.IsNotNull(_player.dungeon.game, "Dungeon " + _player.dungeon.id + " doesn't have a game reference when kicking enemy " + this);
+            }
+
+            DirectionEnum direction = _player.dungeon.game.GetDirection(_currentDungeonId, transform.position);
+           
+            if(_player.dungeon.game.IsAvailableDirection(_currentDungeonId, direction) == false)
+            {
+                StopKick();
+            } else
+            {
+                if(_isWarping == true && direction == DirectionEnum.NONE)
+                {
+                    _isWarping = false;
+                } else if(_isWarping == false && direction != DirectionEnum.NONE)
+                {
+                    int dungeonId = _player.dungeon.game.GetDungeonId(_currentDungeonId, direction);
+                    Vector3 newPosition = _player.dungeon.game.GetPosition(transform.position, direction, _currentDungeonId, dungeonId);
+                    
+                    // Froze warping direction
+                    _isWarping = true;
+                    _warpingDirection = direction;
+
+                    // Set new player and teleport into new room
+                    _agent.Warp(newPosition);
+                }
             }
         }
+    }
+
+    protected void StopKick() {
+        //UnityEngine.Debug.Log("Stop kick ");
+
+        // Reset speed
+        _speed = INITIAL_SPEED / _weight;
+        _isKicked = false;
+        _isWarping = false;
     }
 
     public void Kick(Vector3 direction, float strengh) {

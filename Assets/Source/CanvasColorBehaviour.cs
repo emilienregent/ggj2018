eb92acbaa184e533dc13e61ce620e1cc832d8b98
Maxeee <maxbinetruy@gmail.com>
2018-01-28 14:00:40 +0100

    Add CanvasColorBehaviour

---
+++ Assets/Source/CanvasColorBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(CanvasRenderer))]
+public class CanvasColorBehaviour : ColorBehaviour
+{
+    private Graphic _graphic = null;
+
+    public override void SetFrequency(Frequency frequency)
+    {
+        if (_graphic == null)
+            _graphic = GetComponent<Graphic>();
+
+        Color color = Color.white;
+        if (frequency == Frequency.Hz1)         color = YELLOW * _color;
+        else if (frequency == Frequency.Hz2)    color = RED * _color;
+        else if (frequency == Frequency.Hz3)    color = GREEN * _color;
+        else if (frequency == Frequency.Hz4)    color = BLUE * _color;
+
+        _graphic.color = _color;
+    }
+}
